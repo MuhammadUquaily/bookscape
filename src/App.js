3c5bccc2cb09fb95d348f481554f1f701d4fe84d
Muhammad Uquaily <muqu2023@gmail.com>
2024-11-16 15:29:09 +0500

    Adding functionality to header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { useState, useEffect } from 'react'
 import './App.css';
+import Header from './components/header/header'
 
 function App() {
+
+  const [books, setBooks] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  
+  const handleSearch = async (page = 1, searchTerm = "") => {
+    if (!searchTerm || !searchTerm.trim()) return;
+
+    const limit = 15;
+    try {
+        const response = await fetch(
+            `https://openlibrary.org/search.json?q=${encodeURIComponent(searchTerm)}&limit=${limit}&page=${page}`
+        );
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        setBooks(data.docs);
+        setCurrentPage(page);
+        setHasNextPage(data.docs.length === limit);
+    } catch (error) {
+        console.error('Error during fetch:', error);
+    }
+  }
+
+  useEffect(() => {}, [books]);
+  useEffect(() => {}, [currentPage]);
+  useEffect(() => {}, [hasNextPage]);
+
   return (
     <div className="App">
+      <Header handleSearch={handleSearch} />
     </div>
   );
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,34 @@
+import { React, useState } from "react";
+import './header.css'
+
+const Header = ({ handleSearch }) => {
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value)
+    }
+
+    const handleEnter = (event) => {
+        if (event.key === 'Enter') {
+            handleSearch(1, searchTerm)
+        }
+    }
+
+    return (
+        <div className="header">
+            <input
+                className="search-bar"
+                placeholder="Search for a book"
+                value={searchTerm}
+                onChange={(e) => handleChange(e)}
+                onKeyDown={(e) => handleEnter(e)}
+            />
+            <button
+                onClick={() => handleSearch(1, searchTerm)}
+                className="search-button"
+            />
+        </div>
+    )
+}
+
+export default Header;
